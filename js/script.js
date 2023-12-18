75035edb924507915be845c6c78e1104121866de
LucaB98 <lucabellan98@gmail.com>
2023-12-18 19:21:26 +0100

    scale added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('JS OK');
+
+// 1- Chiedere quanti km vuole percorrere l'utente
+// 2- Chiedere l'eta' dell'utente 
+// 3- Calcolare costo del biglietto
+// 4- Applicare sconto del 20% per i minorenni
+// 5- Applicare sconto del 40% per i over 65
+// 6- calcolare costo biglietto
+// 7- risulato con massimo 2 decimale
+
+
